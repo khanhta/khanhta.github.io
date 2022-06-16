878a562a376de3d0498871e8e1f1cbb1c69bd42b
Kelly Ta <greatleaf3105@yahoo.com>
2022-06-15 21:30:57 -0400

    Make end game condition

--- js/snake.js
+++ js/snake.js
@@ -1,34 +1,50 @@
-const canvas = document.getElementById("snake")
-const context = canvas.getContext("2d")
+const container = document.getElementById("canvas-container"); // to get the screen size
+const canvas = document.getElementById("snake");
+const context = canvas.getContext("2d");
 
-const unit = 32
-const width = 17
-const height = 17
-const center = (width + height) / 2
+const unit = 32;
+const width = Math.floor(container.offsetWidth / unit);
+const height = Math.floor(container.offsetHeight / unit);
+canvas.width = width * unit;
+canvas.height = height * unit;
 
-const ground = new Image()
-ground.src = "img/ground.png"
+const centerX = Math.floor(width / 2);
+const centerY = Math.floor(height / 2);
 
-const foodImage = new Image()
-foodImage.src = "img/food.png"
+const foodImage = new Image();
+foodImage.src = "img/food.png";
 
-const head = "green"
-const body = "white"
+const head = "orange";
+const body = "white";
 
-let snake = []
-snake[0] = {
-	x : center * unit,
-	y : center * unit
+let snake = [];
+let food;
+
+let score;
+
+let direction;
+
+let gameOver = false;
+let speed = 8;
+
+function clearScreen() {
+	context.fillStyle = "black";
+	context.fillRect(0,0, width * unit, height * unit);
 }
 
-let food = {
-		x : Math.floor(Math.random() * width + 1) * unit,
-		y : Math.floor(Math.random() * height + 1) * unit 
-	}
+function setupGame() {
 
-let score = 0
+	snake = [];
+	snake[0] = {
+		x : centerX * unit,
+		y : centerY * unit
+	};
+	score = 0;
+	food = getNewFood();
+	document.addEventListener("keydown", move);
+	setTimeout(draw, speed);
+}
 
-let direction
 
 function move(event) {
 	let key = event.keyCode;
@@ -36,26 +52,39 @@ function move(event) {
 	if (key == 38 && direction != "DOWN") direction = "UP"
 	if (key == 39 && direction != "LEFT") direction = "RIGHT"
 	if (key == 40 && direction != "UP") direction = "DOWN"
-	
+	if (key == 13 && gameOver) {
+		gameOver = false;
+		setupGame();
+	}
 }
 
-document.addEventListener("keydown", move)
+
+function isDeadYet(snakeHead) {
+	if (snakeHead.x < 0 || snakeHead.x == width * unit || snakeHead.y < 0 || snakeHead.y == height * unit) {
+		return true;
+	}
+	for (let i=1; i<snake.length; i++) {
+		if (snakeHead.x == snake[i].x && snakeHead.y == snake[i].y) {
+			return true;
+		}
+	}
+	return false;
+}
 
 function getNewFood() {
-	food = {
-		x : Math.floor(Math.random() * width + 1) * unit,
-		y : Math.floor(Math.random() * height + 1) * unit 
+	return {
+		x : Math.floor(Math.random() * width) * unit,
+		y : Math.floor(Math.random() * height) * unit 
 	}
-	context.drawImage(foodImage, food.x, food.y);
 }	
 
 function draw() {
-	context.drawImage(ground,0,0);
+	clearScreen();
 	for (let i=0; i < snake.length; i++) {
 		context.fillStyle = (i == 0) ? head : body;
 		context.fillRect(snake[i].x, snake[i].y, unit, unit);
 		
-		context.strokeStyle = "red";
+		context.strokeStyle = "black";
 		context.strokeRect(snake[i].x, snake[i].y, unit, unit);
 	}
 	
@@ -74,22 +103,32 @@ function draw() {
 		y : oldHeadY
 	}
 	
+	if (isDeadYet(newHead)) {
+		gameOver = true;
+		context.fillText("Game over. Press Enter to play again", 3 * unit, 3 * unit);
+		return;
+	}
+	
 	snake.unshift(newHead);
 	
 	// snake eats the food
 	if (newHead.x == food.x && newHead.y == food.y) {
-		score += 1
+		score += 1;
 		// remove the food
-		getNewFood()
+		food = getNewFood();
+		context.drawImage(foodImage, food.x, food.y);
+		if (score > 3) speed+=1;
 	} else {
 		// remove the tail because the snake length doesn't change
-		snake.pop()
+		snake.pop();
 	}
 	
 	// draw score
 	context.fillStyle = "white"
-	context.font = "45px Sans serif"
-	context.fillText(score, 3 * unit, 3 * unit)
+	context.font = "10px Sans serif"
+	context.fillText("Score " + score, 3 * unit, 3 * unit)
+	setTimeout(draw, 1000/speed);
 }
 
-let game = setInterval(draw, 100)
+
+setupGame();
